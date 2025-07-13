cdd5d47ce2efa04d7803b43374f08d86e81d2034
waaldiigam <wodzinski93@gmail.com>
2025-07-13 14:42:55 +0200

    Added player, added handling of player move logic

--- main.py
+++ main.py
@@ -3,20 +3,28 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return        
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        player.draw(screen)
+        player.update(dt)                
+        dt = clock.tick(60) / 1000
+        pygame.display.flip()     
+        
         
 if __name__ == "__main__":
     main()
